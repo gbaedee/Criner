cf5042f1329676e9d2476aab6f7493dd7b923049
Great Leh Baedee <greatbaedee@gamil.com>
2018-07-18 14:11:39 -0500

    address bar fixed

--- src/router.js
+++ src/router.js
@@ -61,6 +61,7 @@ import complexSystem from '@/components/logicArtAndTheHumanities/complexSystem'
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
   routes: [
     {
       path: '/',
